36f31d2c34777240830fef770aa9fded9b7c37f3
Nazar Nyrka <nazarnyrka00@gmail.com>
2023-07-17 21:53:44 +0300

    Adding the pagination functionality

--- ClientApp/src/app/shop/shop.component.ts
+++ ClientApp/src/app/shop/shop.component.ts
@@ -3,18 +3,24 @@ import { ShopService } from './shop.service';
 import { Product } from '../shared/models/product';
 import { Brand } from '../shared/models/brand';
 import { Type } from '../shared/models/type';
+import { ShopParams } from '../shared/models/shopParams';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
-  styleUrls: ['./shop.component.scss']
+  styleUrls: ['./shop.component.scss'],
 })
 export class ShopComponent implements OnInit {
   products: Product[] = [];
   brands: Brand[] = [];
   types: Type[] = [];
-  brandIdSelected = 0;
-  typeIdSelected = 0;
+  shopParams = new ShopParams();
+  sortOptions = [
+    { name: 'Alphabetical', value: 'name' },
+    { name: 'Price: Low to high', value: 'priceAsc' },
+    { name: 'Price: High to low', value: 'priceDesc' },
+  ];
+  totalCount = 0;
 
   constructor(private shopService: ShopService) {}
 
@@ -25,30 +31,53 @@ export class ShopComponent implements OnInit {
   }
 
   getProducts() {
-    this.shopService.getProducts(this.brandIdSelected, this.typeIdSelected).subscribe({
-      next: response => this.products = response.data
-    })
+    this.shopService.getProducts(this.shopParams).subscribe({
+      next: (response) => {
+        this.products = response.data;
+        this.shopParams.pageNumber = response.pageNumber;
+        this.shopParams.pageSize = response.pageSize;
+        this.totalCount = response.count;
+      },
+    });
   }
 
   getBrands() {
     this.shopService.getBrands().subscribe({
-      next: response => this.brands = [{id: 0, name: 'All'}, ...response]
-    })
+      next: (response) => (this.brands = [{ id: 0, name: 'All' }, ...response]),
+    });
   }
 
   getTypes() {
     this.shopService.getTypes().subscribe({
-      next: response => this.types = [{id: 0, name: 'All'}, ...response]
-    })
+      next: (response) => (this.types = [{ id: 0, name: 'All' }, ...response]),
+    });
   }
 
   onBrandSelected(brandId: number) {
-    this.brandIdSelected = brandId;
+    this.shopParams.brandId = brandId;
     this.getProducts();
   }
 
   onTypeSelected(typeId: number) {
-    this.typeIdSelected = typeId;
+    this.shopParams.typeId = typeId;
     this.getProducts();
+  }
+
+  onSortSelected(event: any) {
+    this.shopParams.sort = event.target.value;
+    this.getProducts();
+  }
+
+  onPageChange(event: any) {
+    if (this.shopParams.pageNumber !== event.page) {
+      this.shopParams.pageNumber = event.page;
+      this.getProducts();
+
+      const options: ScrollToOptions = {
+        top: 0,
+        behavior: 'smooth'
+      };
+      window.scrollTo(options);
+    }
   }
 }
--- ClientApp/src/app/shop/shop.service.ts
+++ ClientApp/src/app/shop/shop.service.ts
@@ -4,6 +4,7 @@ import { Product } from '../shared/models/product';
 import { Pagination } from '../shared/models/pagination';
 import { Brand } from '../shared/models/brand';
 import { Type } from '../shared/models/type';
+import { ShopParams } from '../shared/models/shopParams';
 
 @Injectable({
   providedIn: 'root'
@@ -13,11 +14,16 @@ export class ShopService {
 
   constructor(private http: HttpClient) { }
 
-  getProducts(brandId?: number, typeId?: number) {
+  getProducts(shopParams: ShopParams) {
     let params = new HttpParams();
 
-    if (brandId) params = params.append('brandId', brandId);
-    if (typeId) params = params.append('typeId', typeId);
+    if (shopParams.brandId > 0) params = params.append('brandId', shopParams.brandId);
+    if (shopParams.typeId > 0) params = params.append('typeId', shopParams.typeId);
+    
+    params = params.append('sort', shopParams.sort);
+    
+    params = params.append('pageNumber', shopParams.pageNumber);
+    params = params.append('pageSize', shopParams.pageSize);
 
     return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products', {params});
   }
