df6835475a90263d2e852cb51b255af35edc07bb
Nazar Nyrka <nazarnyrka00@gmail.com>
2023-07-17 16:15:59 +0300

    Designing the shop page

---
+++ ClientApp/src/app/shop/shop.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Product } from '../shared/models/product';
+import { Pagination } from '../shared/models/pagination';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShopService {
+  baseUrl = 'https://localhost:5001/api/'
+
+  constructor(private http: HttpClient) { }
+
+  getProducts() {
+    return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products?pageSize=50');
+  }
+}
