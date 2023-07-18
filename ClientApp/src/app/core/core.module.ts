3c06d5ea152c13cefa0369ce9449f56ab6545451
Nazar Nyrka <nazarnyrka00@gmail.com>
2023-07-18 21:34:43 +0300

    Adding a footer

--- ClientApp/src/app/core/core.module.ts
+++ ClientApp/src/app/core/core.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    NavBarComponent
+    NavBarComponent,
+    FooterComponent,
   ],
   imports: [
     CommonModule,
@@ -13,7 +15,8 @@ import { RouterModule } from '@angular/router';
   ],
   exports: [
     NavBarComponent,
-    RouterModule
+    RouterModule,
+    FooterComponent
   ]
 })
 export class CoreModule { }
