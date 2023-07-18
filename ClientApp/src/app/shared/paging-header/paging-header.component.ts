83cda3d864cf678b4cc0fe8b5556ddbbbb44b52c
Nazar Nyrka <nazarnyrka00@gmail.com>
2023-07-18 12:04:42 +0300

    Making the pagination-header component a shared

---
+++ ClientApp/src/app/shared/paging-header/paging-header.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { ShopParams } from '../models/shopParams';
+
+@Component({
+  selector: 'app-paging-header',
+  templateUrl: './paging-header.component.html',
+  styleUrls: ['./paging-header.component.scss']
+})
+export class PagingHeaderComponent {
+  @Input() totalCount?: number;
+  @Input() pageNumber?: number;
+  @Input() pageSize?: number;
+}
