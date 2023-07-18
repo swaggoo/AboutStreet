06eb01711da33bdcee8f9bb61f34d4b608ef65be
Nazar Nyrka <nazarnyrka00@gmail.com>
2023-07-18 12:55:52 +0300

    Fixing the bug with paging after filtering

--- ClientApp/src/app/shop/shop.component.ts
+++ ClientApp/src/app/shop/shop.component.ts
@@ -56,21 +56,25 @@ export class ShopComponent implements OnInit {
 
   onBrandSelected(brandId: number) {
     this.shopParams.brandId = brandId;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
   onTypeSelected(typeId: number) {
     this.shopParams.typeId = typeId;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
   onSortSelected(event: any) {
     this.shopParams.sort = event.target.value;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
   onSearch() {
     this.shopParams.search = this.searchTerm?.nativeElement.value;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
