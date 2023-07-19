fbb1140a369e97cd001138903c30ba339b5f47ff
Nazar Nyrka <nazarnyrka00@gmail.com>
2023-07-19 18:26:42 +0300

    Add client-side error handling

--- ClientApp/src/app/core/core.module.ts
+++ ClientApp/src/app/core/core.module.ts
@@ -3,20 +3,31 @@ import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
+import { TestErrorComponent } from './test-error/test-error.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
     NavBarComponent,
     FooterComponent,
+    TestErrorComponent,
+    NotFoundComponent,
+    ServerErrorComponent,
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true
+    })
   ],
   exports: [
     NavBarComponent,
     RouterModule,
-    FooterComponent
+    FooterComponent,
   ]
 })
 export class CoreModule { }
