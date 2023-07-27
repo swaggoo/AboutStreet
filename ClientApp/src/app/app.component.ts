62a3918644255040a76c14d7ab502670ce20aa56
Nazar Nyrka <nazarnyrka00@gmail.com>
2023-07-27 15:29:05 +0300

    Persisting the login

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BasketService } from './basket/basket.service';
+import { AccountService } from './account/account.service';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +8,22 @@ import { BasketService } from './basket/basket.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor(private basketService: BasketService) {}
+
+  constructor(private basketService: BasketService, private accountService: AccountService) {}
 
   ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+
+  loadBasket() {
     const basketId = localStorage.getItem('basket_id');
     if (basketId) this.basketService.getBasket(basketId);
-    
+  }
+
+  loadCurrentUser() {
+    const token = localStorage.getItem('token');
+
+    if(token) this.accountService.loadCurrentUser(token).subscribe();
   }
 }
