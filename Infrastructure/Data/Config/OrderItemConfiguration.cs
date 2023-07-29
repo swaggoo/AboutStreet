6932d2e8ffa57e0df2b184f0a62b06a1595bc482
Nazar Nyrka <nazarnyrka00@gmail.com>
2023-07-29 18:46:31 +0300

    Creating the order entities

---
+++ Infrastructure/Data/Config/OrderItemConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities.OrderAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Config;
+public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+{
+    public void Configure(EntityTypeBuilder<OrderItem> builder)
+    {
+        builder.OwnsOne(i => i.ItemOrdered, io => { io.WithOwner(); });
+
+        builder.Property(i => i.Price)
+            .HasColumnName("decimal(18,2)");
+    }
+}
