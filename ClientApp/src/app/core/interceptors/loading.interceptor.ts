d79a2f3eb0e0de3ff767832fe3e694faa75332b2
Nazar Nyrka <nazarnyrka00@gmail.com>
2023-08-06 17:00:48 +0300

    Refactoring the order creation

--- ClientApp/src/app/core/interceptors/loading.interceptor.ts
+++ ClientApp/src/app/core/interceptors/loading.interceptor.ts
@@ -3,20 +3,27 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
 } from '@angular/common/http';
 import { Observable, delay, finalize } from 'rxjs';
 import { BusyService } from '../services/busy.service';
 
 @Injectable()
 export class LoadingInterceptor implements HttpInterceptor {
-
   constructor(private busyService: BusyService) {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    if (!request.url.includes('emailExists')) {
-      this.busyService.busy();
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    if (
+      request.url.includes('emailExists') ||
+      (request.method == 'POST' && request.url.includes('orders'))
+    ) {
+      return next.handle(request);
     }
+
+    this.busyService.busy();
     return next.handle(request).pipe(
       delay(1000),
       finalize(() => this.busyService.idle())
