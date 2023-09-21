d0b99236daae531fa8d7d5360b846858295382a7
Nazar Nyrka <nazarnyrka00@gmail.com>
2023-09-21 10:08:35 +0300

    Added account controller tests

---
+++ Core/Interfaces/IWrappers/IUserManagerWrapper.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace Core.Interfaces.IWrappers;
+public interface IUserManagerWrapper
+{
+    Task<AppUser> GetUserByClaimsPrinciple(ClaimsPrincipal user);
+    Task<AppUser> GetUserByClaimsPrincipleWithAddress(ClaimsPrincipal user);
+    Task<AppUser> FindByEmailAsync(string email);
+    Task<IdentityResult> UpdateAsync(AppUser user);
+    Task<IdentityResult> CreateAsync(AppUser user, string password);
+}
